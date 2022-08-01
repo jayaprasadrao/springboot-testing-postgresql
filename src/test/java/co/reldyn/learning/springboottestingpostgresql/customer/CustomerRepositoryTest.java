937f5bb988e116158d7f1414d5de3ba797010288
Jaya Prasad Rao <prasad@reldyn.co>
2022-08-01 23:28:28 +0800

    removed unwanted imports

--- src/test/java/co/reldyn/learning/springboottestingpostgresql/customer/CustomerRepositoryTest.java
+++ src/test/java/co/reldyn/learning/springboottestingpostgresql/customer/CustomerRepositoryTest.java
@@ -1,9 +1,7 @@
 package co.reldyn.learning.springboottestingpostgresql.customer;
 
 import com.github.javafaker.Faker;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
